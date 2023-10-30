f93cabfd7d40363b25142b650aa9d96dd1a10a2d
Bruno Sarmento <120613201+BrunoSarmento-dev@users.noreply.github.com>
2023-10-30 16:34:08 -0300

    Api Bookstore 1.0

--- main.py
+++ main.py
@@ -1,31 +1,65 @@
+import json
+from typing import Optional, Literal
+from uuid import uuid4
 from fastapi import FastAPI
+import os
+from fastapi import HTTPException
+import random
+from pydantic import BaseModel
+from fastapi.encoders import jsonable_encoder
 
 app = FastAPI()
 
-book_database = [
-    "Dom Quixote - Miguel de Cervantes",
-    "Cem Anos de Solidão - Gabriel García Márquez",
-    "1984 - George Orwell",
-    "O Pequeno Príncipe - Antoine de Saint-Exupéry",
-    "Harry Potter e a Pedra Filosofal - J.K. Rowling",
-    "Harry Potter e o Prisioneiro de Azkaban - J.K. Rowling"
-]
+
+class Book(BaseModel):
+    name: str
+    price: float
+    id: Optional[str] = uuid4().hex
+    genre: Literal['fiction', 'non-fiction']
+
+
+books_file = 'books.json'
+
+# Maneira anterior de salvar os livros
+book_database = []
+
+
+if os.path.exists(books_file):
+    with open(books_file, 'r') as f:
+        book_database = json.load(f)
+
 
 # /      -> Boas vindas!
 @app.get('/')
 async def home():
     return 'Welcome to my bookstore'
 
+
 # /list-books -> Listar todos os livros
 @app.get('/list-books')
 async def list_books():
     return {'books': book_database}
 
+
 # /list-book-by-index{index} -> listar 1 livro
 @app.get('/list-book-by-index/{index}')
 async def list_book_by_index(index: int):
-    return {'books': book_database [index]}
+    if index < 0 or index >= len(book_database):
+        raise HTTPException(404, "Index out of range")  # Error
+    return {'books': book_database[index]}
 
+
+# /get-random-book -> livro aleátorio
+@app.get('/get-random-book')
+async def get_random_book():
+    return random.choice(book_database)
 
 
 # /add-book -> adicionar novo livro
+@app.post('/add-book')
+async def add_book(book: Book):
+    json_book = jsonable_encoder(book)
+    book_database.append(json_book)
+    with open(books_file, 'w') as f:
+        json.dump(book_database, f)
+    return f' Book {book} was added'
